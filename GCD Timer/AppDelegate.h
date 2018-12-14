9de96663ad63c7633130e91e01798c06f8bf4e78
袁鑫亮 <306619887@qq.com>
2018-12-14 13:35:21 +0800

    Initial Commit

---
+++ GCD Timer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCD Timer
+//
+//  Created by XL Yuen on 2018/12/14.
+//  Copyright © 2018 XL Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCD Timer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCD Timer
+//
+//  Created by XL Yuen on 2018/12/14.
+//  Copyright © 2018 XL Yuen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
