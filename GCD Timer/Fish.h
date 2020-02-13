9803813b097adf296f120fe272c4e5a236937030
袁鑫亮 <306619887@qq.com>
2020-02-13 23:56:33 +0800

    c

---
+++ GCD Timer/Fish.h
@@ -0,0 +1,17 @@
+//
+//  Fish.h
+//  GCD Timer
+//
+//  Created by XL Yuen on 2020/2/13.
+//  Copyright © 2020 XL Yuen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Fish : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GCD Timer/Fish.m
@@ -0,0 +1,13 @@
+//
+//  Fish.m
+//  GCD Timer
+//
+//  Created by XL Yuen on 2020/2/13.
+//  Copyright © 2020 XL Yuen. All rights reserved.
+//
+
+#import "Fish.h"
+
+@implementation Fish
+
+@end
